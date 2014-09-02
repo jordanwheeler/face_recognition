67238d0aef63f43268d282c36735345876febb22
Jordan Wheeler <jordan.wheeler@jadedpixel.com>
2014-09-02 14:01:27 -0400

    testing github webhooks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  # TESTING GITHUB WEBHOOKS
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,3 @@
 module ApplicationHelper
+  # MORE TESTING OF GITHUB WEBHOOKS
 end
